b0046a620f80dfce220defbf078be585bfbc37d5
1894475 <1894475@LHB10442.MATRIXCOLLEGE.LCL>
2019-10-19 13:56:34 -0400

    countries - sukh has completed all the functions

--- JavaWebActivity/src/java/com/restdb/Countries.java
+++ JavaWebActivity/src/java/com/restdb/Countries.java
@@ -105,4 +105,186 @@ public class Countries {
         }
     }
    
+    //Insert into database
+ @GET
+ @Path("insertDB&{cID}&{cName}&{rID}")
+ @Produces(MediaType.APPLICATION_JSON)
+ public String getText1(@PathParam("cID") int CID, @PathParam("cName") String CName, @PathParam("rID") int rID) throws SQLException, ClassNotFoundException{
+
+       try {
+            Class.forName("oracle.jdbc.OracleDriver");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "hr", "inf5180");
+            
+            
+            String sql = "INSERT INTO countries(country_id,country_name,region_id) values (?, ?, ?)";
+            ps = con.prepareStatement(sql);
+            
+            ps.setInt(1,CID);
+            ps.setString(2,CName);
+            ps.setInt(3,rID);
+            
+            int flag = ps.executeUpdate();
+
+           
+            if(flag==1)
+            {
+                
+                mainObject.accumulate("Message","Data Insertion Sucessful");
+                mainObject.accumulate("Status","OK");
+                mainObject.accumulate("Timestamp",now);
+                
+            }
+   
+        } catch (SQLException ex) {
+            mainObject.accumulate("Message","Data not Inserted");
+            mainObject.accumulate("Status","ERROR");
+            mainObject.accumulate("Timestamp",now);
+                
+            Logger.getLogger(DepartmentTable.class.getName()).log(Level.SEVERE, null, ex);
+        } 
+        catch (ClassNotFoundException ex) { 
+            Logger.getLogger(DepartmentTable.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return mainObject.toString();
+    
+ }
+
+ //Display single List of countries
+ @GET
+ @Path("singleDB&{cID}")
+ @Produces(MediaType.APPLICATION_JSON)
+ public String getText2(@PathParam("cID") int cID){
+
+    try {
+        Class.forName("oracle.jdbc.OracleDriver");
+        con = DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "hr", "inf5180");
+        
+        String singlelist = "select * from countries where country_id= '"+cID+"'";
+        ps = con.prepareStatement(singlelist);
+        rs = ps.executeQuery();
+        
+        if(!rs.next()){
+            mainObject.accumulate("Message","Data not Found!");
+            mainObject.accumulate("Status","ERROR");
+            mainObject.accumulate("Timestamp",now);
+        }
+        else
+        {
+        
+    do
+        {
+            int c_id, r_id;
+            String c_name;
+            
+            c_id = rs.getInt("country_id");
+            r_id = rs.getInt("region_id");
+            c_name = rs.getString("country_name");
+            
+            mainObject.accumulate("Country ID", c_id);
+            mainObject.accumulate("Region ID", r_id);
+            mainObject.accumulate("Country name", c_name);
+            mainObject.accumulate("Status","OK");
+            mainObject.accumulate("Timestamp",now);
+        
+            } while (rs.next());
+        }
+  
+    } 
+    
+    catch (SQLException | ClassNotFoundException ex) {
+        Logger.getLogger(DepartmentTable.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    finally {
+            closeDBConnection(rs, ps, con);
+            }
+        return mainObject.toString();
+    }
+
+    //UPDATE single row of countries
+ @GET
+ @Path("updateDB&{cID}&{cName}")
+ @Produces(MediaType.APPLICATION_JSON)
+ public String getText3(@PathParam("cID") int cID,@PathParam("cName") String cName){
+
+    try {
+        Class.forName("oracle.jdbc.OracleDriver");
+        con = DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "hr", "inf5180");
+        
+       
+        String updatelist = "update countries set country_name= ? where country_id=?";
+        
+        
+        ps = con.prepareStatement(updatelist);
+        ps.setInt(2, cID);
+        ps.setString(1,cName);
+        
+        int flag = ps.executeUpdate();
+    
+        if(flag!=1){
+            mainObject.accumulate("Message","Data not Found!");
+            mainObject.accumulate("Status","ERROR");
+            mainObject.accumulate("Timestamp",now);
+        }
+        else
+        {
+       
+            mainObject.accumulate("Message", "Data updated Sucessfully!");
+            mainObject.accumulate("Status","OK");
+            mainObject.accumulate("Timestamp",now);
+  
+        }
+  
+    } 
+    
+    catch (SQLException | ClassNotFoundException ex) {
+            mainObject.accumulate("Message","Data not Found!");
+            mainObject.accumulate("Status","ERROR");
+            mainObject.accumulate("Timestamp",now);
+        Logger.getLogger(DepartmentTable.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    finally {
+            closeDBConnection(rs, ps, con);
+            }
+        return mainObject.toString();
+    }
+ 
+  //Delete single List of Departments
+ @GET
+ @Path("deleteDB&{cID}")
+ @Produces(MediaType.APPLICATION_JSON)
+ public String getText4(@PathParam("cID") int cID){
+
+    try {
+        Class.forName("oracle.jdbc.OracleDriver");
+        con = DriverManager.getConnection("jdbc:oracle:thin:@144.217.163.57:1521:XE", "hr", "inf5180");
+        
+        String deletelist = "delete from countries where country_id = ?";
+       
+        
+        ps = con.prepareStatement(deletelist);
+        ps.setInt(1,cID);
+        int flag = ps.executeUpdate();
+        
+        
+        if(flag==1){
+            mainObject.accumulate("Message","Data Deleted Sucessfully!");
+            mainObject.accumulate("Status","ERROR");
+            mainObject.accumulate("Timestamp",now);
+        }
+       
+  
+    } 
+    
+    catch (SQLException | ClassNotFoundException ex) {
+            mainObject.accumulate("Message", "Data not Deleted!");
+            mainObject.accumulate("Status","Error!");
+            mainObject.accumulate("Timestamp",now);
+        Logger.getLogger(DepartmentTable.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    finally {
+            closeDBConnection(rs, ps, con);
+            }
+        return mainObject.toString();
+    }
 }
